9aac18265ba60f1d96583e0182276ac0853ff443
Yunu <gyunu47@gmail.com>
2023-01-26 20:50:53 +0900

    [#1] chore: emotion 설치 및 설정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,17 @@
+import { css } from '@emotion/react';
+
 export default function Home() {
-  return <h1>hello next!</h1>;
+  return (
+    <h1
+      css={css({
+        color: 'red',
+      })}
+    >
+      hello next!
+    </h1>
+  );
 }
+
+// export default function Home() {
+//   return <h1>hello next!</h1>;
+// }
