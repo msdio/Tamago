799a9f259b0c8acb784856a5fa86f0fa55703683
wonseok <tkddls23@konkuk.ac.kr>
2023-01-31 01:36:01 +0900

    [#2] feat: backend 폴더 생성 및 spring 설정

---
+++ src/backend/src/main/java/com/project/Tamago/TamagoApplication.java
@@ -0,0 +1,13 @@
+package com.project.Tamago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagoApplication.class, args);
+	}
+
+}
