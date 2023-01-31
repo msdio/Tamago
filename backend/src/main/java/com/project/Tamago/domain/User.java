5eba9033b718cbc6dae229bbfbc2c04016c59b52
wonseok <tkddls23@konkuk.ac.kr>
2023-02-01 02:37:49 +0900

    [#7] feat: user 테이블 생성

---
+++ backend/src/main/java/com/project/Tamago/domain/User.java
@@ -0,0 +1,51 @@
+package com.project.Tamago.domain;
+
+import com.project.Tamago.util.converter.RoleConverter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 10, nullable = false, unique = true)
+    @Size(max = 10)
+    private String nickname;
+
+    @Size(max = 20)
+    private String introduce;
+
+    private String profileImg;
+
+    @ColumnDefault("true")
+    private Boolean terms;
+
+    @ColumnDefault("true")
+    private Boolean status;
+
+    @ColumnDefault("'none'")
+    private String provider;
+
+    private String providerId;
+
+    @Convert(converter = RoleConverter.class)
+    private Role role;
+}
