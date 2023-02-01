5e8dd66c4c3b4a3ec21f1016a407a749370836ae
Yunu <gyunu47@gmail.com>
2023-02-01 16:24:40 +0900

    [#1] chore: chakra-ui 설치 및 동작 확인

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,16 @@
-import { css } from '@emotion/react';
+// import { css } from '@emotion/react';
+import { Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <h1
-      css={css({
-        color: 'red',
-      })}
-    >
-      hello next!
-    </h1>
+    // <h1
+    //   css={css({
+    //     color: 'red',
+    //   })}
+    // >
+    //   hello next!
+    // </h1>
+    <Box w='50px' h='50px' bg='tomato' />
   );
 }
 
