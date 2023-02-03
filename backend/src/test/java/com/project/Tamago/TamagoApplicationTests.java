531478c4441ba10331503abfd5a9b784d7a51e81
jhbaik1501 <jhbaik1501@gmail.com>
2023-02-04 04:13:09 +0900

    test: mockMvc를 활용한 test 코드 작성

--- backend/src/test/java/com/project/Tamago/TamagoApplicationTests.java
+++ backend/src/test/java/com/project/Tamago/TamagoApplicationTests.java
@@ -1,13 +1,58 @@
 package com.project.Tamago;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class TamagoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testTestMvc1() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/test?str=1")
+				.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().isOk())
+			.andExpect(content().string("Success"))
+			.andDo(print());
+	}
+
+	@Test
+	void testTestMvc2() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/test?str=user")
+				.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().is4xxClientError())
+			.andExpect(jsonPath("$.code", notNullValue()))
+			.andExpect(jsonPath("$.message", notNullValue()))
+			.andDo(print());
+	}
+
+	@Test
+	void testTestMvc3() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/test?str=error")
+				.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(status().is5xxServerError())
+			.andExpect(jsonPath("$.code", notNullValue()))
+			.andExpect(jsonPath("$.message", notNullValue()))
+			.andDo(print());
 	}
 
 }
