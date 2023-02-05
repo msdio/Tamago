9c55f466a9830d770bb3e862bda99f44fdd0088d
sumi-0011 <selina2000@naver.com>
2023-02-05 19:25:40 +0900

    [#12] feat: login page layout create

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,48 +1,66 @@
 import Image from 'next/image';
-import { Box, Button, Checkbox, Flex, Grid, GridItem, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import LoginLayout from '../../component/login/layout';
+import LoginForm from '../../component/login/login-form';
 
 export default function LoginPage() {
   return (
-    <div>
-      <header>header</header>
-      <Grid templateColumns='1fr 1fr' h='calc(100vh - 100px)' alignItems='center' gap='100px' p='150px'>
-        <Flex flexDirection='column'>
-          <Heading fontSize='6xl' as='h1'>
-            TAMAGO
-          </Heading>
-          <Text size='4xl' mt='2'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim venis
-          </Text>
-          <Box>
-            <Button size='lg' mt='12'>
-              <Flex justifyContent='space-between' w='200px'>
-                <Text>Go</Text>
-                {/* <div> -></div> */}
-                <Image src={'/image/arrow-right.png'} width={15} height={15} alt={''} />
-              </Flex>
-            </Button>
-          </Box>
-        </Flex>
-        <Flex flexDirection='column' gap={4}>
-          <Heading as='h2' size='md'>
-            Login
-          </Heading>
-          <Input type='email' name='email' id='email' placeholder='E-mail' />
-          <Input type='password' name='password' id='password' placeholder='Password' />
-          <Flex justifyContent='space-between'>
-            <Checkbox>Remember</Checkbox>
-            <Text>Forgot Password</Text>
-          </Flex>
-          <Button bg={'#BABABA'}>Sign in</Button>
-          <Button bg='#fff'>Sign up</Button>
+    <LoginLayout>
+      <Tabs variant='unstyled'>
+        <TabList>
+          <LoginTab>로그인</LoginTab>
+          <LoginTab>게스트</LoginTab>
+        </TabList>
 
-          <Flex justifyContent='center' gap={4}>
-            <Button></Button>
-            <Button></Button>
-          </Flex>
-        </Flex>
-      </Grid>
-    </div>
+        <TabPanels>
+          <TabPanelWrapper>
+            <LoginForm />
+          </TabPanelWrapper>
+          <TabPanelWrapper>
+            <p>two!</p>
+          </TabPanelWrapper>
+        </TabPanels>
+      </Tabs>
+    </LoginLayout>
+  );
+}
+
+function LoginTab({ children }: { children: string }) {
+  return (
+    <Tab
+      _selected={{ borderBottom: '5px solid #FF8A65', fontWeight: 700 }}
+      borderBottom='5px solid #fff'
+      padding={0}
+      marginRight={6}
+    >
+      {children}
+    </Tab>
+  );
+}
+
+function TabPanelWrapper({ children }: { children: ReactNode }) {
+  return (
+    <TabPanel px={0}>
+      <Flex flexDirection='column' gap={4} maxW='486px' w='486px' h={'600px'} p={0}>
+        {children}
+      </Flex>
+    </TabPanel>
   );
 }
