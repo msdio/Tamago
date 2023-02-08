6f79eee100cb137e3d1c114f80917863b9afbd77
msdio <rlagofka1@gmail.com>
2023-02-08 22:18:43 +0900

    [#25] feat: eslint, prettier, vscode editor 설정

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,19 +1,3 @@
-// import { css } from '@emotion/react';
-import { Box } from '@chakra-ui/react';
-
 export default function Home() {
-  return (
-    // <h1
-    //   css={css({
-    //     color: 'red',
-    //   })}
-    // >
-    //   hello next!
-    // </h1>
-    <Box w='50px' h='50px' bg='tomato' />
-  );
+  return <></>;
 }
-
-// export default function Home() {
-//   return <h1>hello next!</h1>;
-// }
