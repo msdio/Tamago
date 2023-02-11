b4c1d94889de2201846f065fcbae427edd0f5ad4
wonseok <tkddls23@konkuk.ac.kr>
2023-02-11 20:58:50 +0900

    feat: auth controller service 생성

---
+++ backend/src/main/java/com/project/Tamago/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.project.Tamago.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+}
---
+++ backend/src/main/java/com/project/Tamago/service/AuthService.java
@@ -0,0 +1,4 @@
+package com.project.Tamago.service;
+
+public class AuthService {
+}
