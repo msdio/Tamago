c6d75730f95eb3a35caa0c85709afd6bf5c4a009
jhbaik1501 <jhbaik1501@gmail.com>
2023-02-14 06:05:32 +0900

    fix : 도커를 background에서 실행할 수 있도록 변경, 만약 이전에 실행되고 있었던 도커 컨테이너가 존재한다면 멈추고 삭제

--- dockerfile/backend/backend_install.sh
+++ dockerfile/backend/backend_install.sh
@@ -1,4 +1,5 @@
 # docker build --build-arg DISABLE_CACHE=$CUR_TIME -t test:1.3 .
 sudo docker build --no-cache -t test:1.3 .
-sudo docker rm release1
-sudo docker run -p 8080:9000/tcp -i -t --name release1 test:1.3
+if [ -z $(sudo docker ps -a -f "name=release1" -q) ]; then echo "null input"; else sudo docker stop release1
+sudo docker rm release1; fi
+sudo docker run -d -p 8080:9000/tcp -i --name release1 test:1.3 sleep infinity
