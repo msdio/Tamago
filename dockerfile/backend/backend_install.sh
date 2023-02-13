cc96cc90b6679e895d4b51a5d50de568b328bcfd
jhbaik1501 <jhbaik1501@gmail.com>
2023-02-14 06:17:36 +0900

    fix : 실행 안되는 오류 제거

--- dockerfile/backend/backend_install.sh
+++ dockerfile/backend/backend_install.sh
@@ -2,4 +2,4 @@
 sudo docker build --no-cache -t test:1.3 .
 if [ -z $(sudo docker ps -a -f "name=release1" -q) ]; then echo "null input"; else sudo docker stop release1
 sudo docker rm release1; fi
-sudo docker run -d -p 8080:9000/tcp -i --name release1 test:1.3 sleep infinity
+sudo docker run -d -p 8080:9000/tcp -i --name release1 test:1.3
