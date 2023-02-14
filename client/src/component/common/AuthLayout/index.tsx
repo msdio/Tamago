c1a31ed4f33e1375f973af7770529727b12c4994
sumi-0011 <selina2000@naver.com>
2023-02-14 23:52:04 +0900

    feat: 비밀번호 찾기 main ui

--- client/src/component/common/AuthLayout/index.tsx
+++ client/src/component/common/AuthLayout/index.tsx
@@ -1,17 +1,26 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
 
 interface LayoutProps {
   title: string;
   children: ReactNode;
+  desc?: string;
 }
 
-export default function AuthLayout({ children, title }: LayoutProps) {
+export default function AuthLayout({ children, title, desc }: LayoutProps) {
   return (
-    <Flex as='main' direction='column' w='490px' m='171px auto 288px auto'>
-      <Heading fontSize='28px' fontWeight='700' textAlign='center' mb='46px'>
-        {title}
-      </Heading>
+    <Flex as='main' direction='column' w='490px' m='142px auto 288px auto'>
+      <Box mb='46px'>
+        <Heading as='h1' fontSize='28px' fontWeight='700' textAlign='center'>
+          {title}
+        </Heading>
+        {desc ? (
+          <Heading as='h3' textAlign='center' fontSize='17px' fontWeight='700' mt='26px'>
+            {desc}
+          </Heading>
+        ) : null}
+      </Box>
+
       {children}
     </Flex>
   );
--- client/src/component/findPassword/index.tsx
+++ client/src/component/findPassword/index.tsx
@@ -1,3 +1,61 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
+
+import AuthLayout from '@/component/common/AuthLayout';
+import RegexInput from '@/components/signup/Form/RegexInput';
+import { EMAIL_REGEX } from '@/utils/regex';
+
 export default function FindPassword() {
-  return <div>FindPassword</div>;
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+  });
+
+  const { name, email } = inputs;
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  return (
+    <AuthLayout title='비밀번호 찾기' desc='가입하신 이메일을 입력하신 뒤 인증을 완료해 주세요.'>
+      <Box mb='26px'>
+        <RegexInput
+          label='이름'
+          size='lg'
+          name='name'
+          placeholder='이름을 입력해 주세요.'
+          value={name}
+          onChange={handleInputChange}
+        />
+      </Box>
+      <Box mb='57px'>
+        <RegexInput
+          label='이메일'
+          size='lg'
+          name='email'
+          type='email'
+          placeholder='이메일을 입력해 주세요.'
+          regex={EMAIL_REGEX}
+          errorMessage='이메일 형식을 확인해 주세요.'
+          value={email}
+          onChange={handleInputChange}
+        />
+      </Box>
+
+      <Button colorScheme='secondary' size='lg'>
+        인증번호 받기
+      </Button>
+
+      <Flex gap='10px' justifyContent='center' mt='24px'>
+        <Text color='#808080'>계정이 없으신가요?</Text>
+        <Link href='/signup'>
+          <Text>회원가입하기</Text>
+        </Link>
+      </Flex>
+    </AuthLayout>
+  );
 }
