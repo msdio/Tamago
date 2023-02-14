a140f55ccf5e069dc747db279afdc056fa247fd1
sumi-0011 <selina2000@naver.com>
2023-02-14 23:43:42 +0900

    refactor: common auth layout 분리

--- client/src/component/common/AuthLayout/index.tsx
+++ client/src/component/common/AuthLayout/index.tsx
@@ -1,15 +1,16 @@
 import { Flex, Heading } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
 
-interface SignupLayoutProps {
+interface LayoutProps {
+  title: string;
   children: ReactNode;
 }
 
-export default function SignupLayout({ children }: SignupLayoutProps) {
+export default function AuthLayout({ children, title }: LayoutProps) {
   return (
     <Flex as='main' direction='column' w='490px' m='171px auto 288px auto'>
       <Heading fontSize='28px' fontWeight='700' textAlign='center' mb='46px'>
-        회원가입
+        {title}
       </Heading>
       {children}
     </Flex>
--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 
+import AuthLayout from '@/component/common/AuthLayout';
+
 import SignupForm from '../components/signup/Form';
-import SignupLayout from '../components/signup/Layout';
 import SignupTerms from '../components/signup/Terms';
 
 export default function SignupPage() {
   const [agreeTerms, setAgreeTerms] = useState(false);
 
-  return <SignupLayout>{agreeTerms ? <SignupForm /> : <SignupTerms setAgreeTerms={setAgreeTerms} />}</SignupLayout>;
+  return (
+    <AuthLayout title={'회원가입'}>
+      {agreeTerms ? <SignupForm /> : <SignupTerms setAgreeTerms={setAgreeTerms} />}
+    </AuthLayout>
+  );
 }
