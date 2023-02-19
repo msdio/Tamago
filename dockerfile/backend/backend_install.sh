456e0e0d81fce6d8fd9886fd3aa2365900e6f1d2
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2023-02-19 16:31:07 +0900

    fix: release를 위한 docker 관련 shell script 변경

--- dockerfile/backend/backend_install.sh
+++ dockerfile/backend/backend_install.sh
@@ -1,5 +1,6 @@
-# docker build --build-arg DISABLE_CACHE=$CUR_TIME -t test:1.3 .
-sudo docker build --no-cache -t test:1.3 .
-if [ -z $(sudo docker ps -a -f "name=release1" -q) ]; then echo "null input"; else sudo docker stop release1
+sudo docker pull baikjonghyun/tamago:lastest
+if [ -z $(sudo docker ps -a -f "name=release1" -q) ]; then echo "null input"; 
+else sudo docker stop release1
 sudo docker rm release1; fi
-sudo docker run -d -p 8080:9000/tcp -i --name release1 test:1.3
+sleep 3
+sudo docker run -d -p 8080:9000/tcp -i --name release1 baikjonghyun/tamago:lastest
