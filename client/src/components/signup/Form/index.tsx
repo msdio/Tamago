e74bf6e8ab4326ed58d665025a291beb050cf065
Yunu <gyunu47@gmail.com>
2023-02-19 16:52:18 +0900

    [#36] feat: Alert 크기 조정, 이메일 확인 alert 추가

--- client/src/components/signup/Form/index.tsx
+++ client/src/components/signup/Form/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Flex, Image } from '@chakra-ui/react';
+import { Box, Button, Flex, Image, Text, useDisclosure } from '@chakra-ui/react';
 
+import Alert from '@/components/common/Alert';
 import FormOr from '@/components/common/FormOr';
 import RegexInput from '@/components/common/RegexInput';
 import useRegexInputs from '@/hooks/useRegexInputs';
@@ -8,6 +9,8 @@ import { EMAIL_REGEX, PASSWORD_REGEX } from '@/utils/regex';
 import EmailInput from './EmailInput';
 
 export default function SignupForm() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [inputs, valids, handleInputChange] = useRegexInputs({
     name: /./,
     email: EMAIL_REGEX,
@@ -27,72 +30,79 @@ export default function SignupForm() {
   };
 
   return (
-    <Flex direction='column'>
-      <Box mb='25px'>
-        <RegexInput
-          label='이름'
-          size='lg'
-          name='name'
-          placeholder='이름을 입력해 주세요.'
-          value={name}
-          isValid={isNameValid}
-          onChange={handleInputChange}
-        />
-      </Box>
-      <Box mb='26px'>
-        <EmailInput
-          label='이메일'
-          size='md'
-          name='email'
-          type='email'
-          placeholder='이메일을 입력해 주세요.'
-          errorMessage='이메일 형식을 확인해 주세요.'
-          value={email}
-          isValid={isEmailValid}
-          onChange={handleInputChange}
-          onClick={handleEmailButton}
-          buttonText='중복 확인'
-        />
-      </Box>
-      <Box mb='16px'>
-        <RegexInput
-          label='비밀번호'
-          size='lg'
-          name='password'
-          type='password'
-          placeholder='8-12자 영문 + 숫자를 포함하여 입력해 주세요.'
-          errorMessage='8-12자 영문 + 숫자를 포함하여 입력해 주세요.'
-          value={password}
-          isValid={isPasswordValid}
-          onChange={handleInputChange}
-        />
-      </Box>
-      <Box mb='57px'>
-        <RegexInput
-          size='lg'
-          name='verifyPassword'
-          type='password'
-          placeholder='비밀번호를 한 번 더 입력해 주세요.'
-          errorMessage='비밀번호가 일치하지 않습니다.'
-          value={verifyPassword}
-          isValid={password === verifyPassword}
-          onChange={handleInputChange}
-        />
-      </Box>
-      <Button size='lg' onClick={handleSubmit}>
-        회원가입 하기
-      </Button>
-      <Box m='41px 0px'>
-        <FormOr />
-      </Box>
-      <Flex justifyContent='center' gap={4}>
-        <Button bg='fff' border='0.6px solid #BFBFBF' width='59px' height='59px' p={0}>
-          <Image src='/images/google-icon.svg' alt='google login' width={40} height={38} />
+    <>
+      <Flex direction='column'>
+        <Box mb='25px'>
+          <RegexInput
+            label='이름'
+            size='lg'
+            name='name'
+            placeholder='이름을 입력해 주세요.'
+            value={name}
+            isValid={isNameValid}
+            onChange={handleInputChange}
+          />
+        </Box>
+        <Box mb='26px'>
+          <EmailInput
+            label='이메일'
+            size='md'
+            name='email'
+            type='email'
+            placeholder='이메일을 입력해 주세요.'
+            errorMessage='이메일 형식을 확인해 주세요.'
+            value={email}
+            isValid={isEmailValid}
+            onChange={handleInputChange}
+            onClick={handleEmailButton}
+            buttonText='중복 확인'
+          />
+        </Box>
+        <Box mb='16px'>
+          <RegexInput
+            label='비밀번호'
+            size='lg'
+            name='password'
+            type='password'
+            placeholder='8-12자 영문 + 숫자를 포함하여 입력해 주세요.'
+            errorMessage='8-12자 영문 + 숫자를 포함하여 입력해 주세요.'
+            value={password}
+            isValid={isPasswordValid}
+            onChange={handleInputChange}
+          />
+        </Box>
+        <Box mb='57px'>
+          <RegexInput
+            size='lg'
+            name='verifyPassword'
+            type='password'
+            placeholder='비밀번호를 한 번 더 입력해 주세요.'
+            errorMessage='비밀번호가 일치하지 않습니다.'
+            value={verifyPassword}
+            isValid={password === verifyPassword}
+            onChange={handleInputChange}
+          />
+        </Box>
+        <Button size='lg' onClick={handleSubmit}>
+          회원가입 하기
         </Button>
-        <Button bg='fff' border='0.6px solid #BFBFBF' width='59px' height='59px' p={0}>
-          <Image src='/images/github-icon.svg' alt='google login' width={40} height={38} />
-        </Button>
+        <Box m='41px 0px'>
+          <FormOr />
+        </Box>
+        <Flex justifyContent='center' gap={4}>
+          <Button bg='fff' border='0.6px solid #BFBFBF' width='59px' height='59px' p={0}>
+            <Image src='/images/google-icon.svg' alt='google login' width={40} height={38} />
+          </Button>
+          <Button bg='fff' border='0.6px solid #BFBFBF' width='59px' height='59px' p={0}>
+            <Image src='/images/github-icon.svg' alt='google login' width={40} height={38} />
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+      <Alert isOpen={true} onClose={onClose}>
+        <Box textAlign='center' mt='34px' mb='54px'>
+          <Text fontWeight='700'>사용 가능한 이메일 입니다.</Text>
+        </Box>
+      </Alert>
+    </>
   );
 }
