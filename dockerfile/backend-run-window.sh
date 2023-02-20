b926a2745f655e7e345ba3b0318377ef2147ef02
jhbaik1501 <jhbaik1501@gmail.com>
2023-02-20 15:31:38 +0900

    fix : shell script를 통한 docker를 통한 서버 띄우기 자동화

--- dockerfile/backend-run-window.sh
+++ dockerfile/backend-run-window.sh
@@ -3,7 +3,7 @@ if [ -z $(docker ps -a -f "name=some-redis" -q) ]; then echo "null input-redis";
 else docker stop some-redis
 docker rm some-redis; fi
 sleep 1
-winpty docker run --name some-redis -d -p 6379:6379 redis
+docker run --name some-redis -d -p 6379:6379 redis
 
 sleep 1
 
@@ -12,4 +12,4 @@ if [ -z $(docker ps -a -f "name=release1" -q) ]; then echo "null input-tamago-lo
 else docker stop release1
 docker rm release1; fi
 sleep 1
-winpty docker run -d -p 8080:9000/tcp -i --name release1 baikjonghyun/tamago:lastest
+docker run -d -p 8080:9000/tcp -i --name release1 baikjonghyun/tamago:lastest
