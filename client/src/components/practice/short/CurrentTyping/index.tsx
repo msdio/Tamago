5642c33e1fbbdfd54e78b30c133d369077bad09e
sumi-0011 <selina2000@naver.com>
2023-03-01 03:32:00 +0900

    feat: 한글 틀린 단어 체크

--- client/src/components/practice/short/CurrentTyping/index.tsx
+++ client/src/components/practice/short/CurrentTyping/index.tsx
@@ -2,10 +2,55 @@ import { Box, Input, Text } from '@chakra-ui/react';
 import type { ChangeEvent } from 'react';
 import { useState } from 'react';
 
+import type { ShortTypingType } from '@/apis/typing';
+import { checkEqualHangul, isHangulChar } from '@/utils/hangul';
+
+type ErrorWordType = Record<string, number>;
+
 interface CurrentTypingProps {
   writing: ShortTypingType;
 }
+
+const mergeObj = (obj1: ErrorWordType, obj2: ErrorWordType) => {
+  const newObj = { ...obj1 };
+  for (const key in obj2) {
+    newObj[key] += obj1[key];
+  }
+
+  return newObj;
+};
+
+// NOTE:
+// 1. 글자가 끝났는지 확인한다
+// 2. 글자가 끝났다면, 글자가 틀렸는지 확인한다.
+// 3. 틀렸다면, 에러 단어에 추가한다.
+
 export default function CurrentTyping({ writing }: CurrentTypingProps) {
+  const [input, setInput] = useState('');
+  const [errorWordList, setErrorWordList] = useState<ErrorWordType[]>([]);
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const word = e.target.value;
+    setInput(word);
+
+    const currentIdx = word.length - 1;
+    const lastWord = word[currentIdx];
+    const correctLastWord = writing.content[currentIdx];
+
+    // NOTE: 한글인 경우
+    const isHangul = isHangulChar(lastWord);
+
+    if (isHangul) {
+      const currentErrorWords = checkEqualHangul(correctLastWord, lastWord);
+      // idx마다 따로 체크해야할것 같다고 생각이 든다.
+      // 치는 도중에 에러가 발생하나?
+      const prevErrorWords = [...errorWordList];
+      console.log('prevErrorWords: ', prevErrorWords);
+      prevErrorWords[currentIdx] = currentErrorWords;
+      setErrorWordList(prevErrorWords);
+    }
+  };
+
   return (
     <Box p='30px 49px'>
       <Box pl='5px'>
--- client/src/utils/hangul.ts
+++ client/src/utils/hangul.ts
@@ -16,3 +16,30 @@ export const getConstantVowel = (kor: string) => {
     t: FINAL_CONSONANT[tn],
   };
 };
+
+export function isHangulChar(word: string) {
+  if (!word) return false;
+
+  const c = word.charCodeAt(0);
+  if (0x1100 <= c && c <= 0x11ff) return true;
+  if (0x3130 <= c && c <= 0x318f) return true;
+  if (0xac00 <= c && c <= 0xd7a3) return true;
+  return false;
+}
+
+export const checkEqualHangul = (word1: string, word2: string) => {
+  const { f: f1, s: s1, t: t1 } = getConstantVowel(word1);
+  const { f: f2, s: s2, t: t2 } = getConstantVowel(word2);
+  const errorWords: Record<string, number> = {};
+  if (f1 !== f2) {
+    errorWords[f1] = 1;
+  }
+  if (s1 !== s2) {
+    errorWords[s1] = 1;
+  }
+  if (t1 !== t2) {
+    errorWords[t1] = errorWords[t1] ? errorWords[t1] + 1 : 1;
+  }
+
+  return errorWords;
+};
