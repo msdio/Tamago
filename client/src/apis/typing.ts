02c7d5a2c9e778ceea75c3f06d86e60ba89d09fa
sumi-0011 <selina2000@naver.com>
2023-03-01 03:25:58 +0900

    feat: dummy short api 추가

---
+++ client/src/apis/typing.ts
@@ -0,0 +1,35 @@
+export interface ShortTypingType {
+  typingId: string;
+  content: string;
+}
+
+const SHORT_DUMMY = {
+  contentType: '0', // 1: 긴글?, 0: 짧은 글
+  typingsType: 'practice',
+  typingWritings: [
+    {
+      typingId: '10',
+      content: '가는 말이 고와야 오는 말도 곱다',
+    },
+    {
+      typingId: '21',
+      content: '소 잃고 외양간 고치기',
+    },
+    {
+      typingId: '13',
+      content: '소 같이 일한다',
+    },
+    {
+      typingId: '22',
+      content: '닭 소 보듯 소 닭 보듯',
+    },
+    {
+      typingId: '50',
+      content: '소 귀에 경 읽기',
+    },
+  ],
+};
+
+export const getShortTypingWritingsAPI = async (): Promise<ShortTypingType[]> => {
+  return SHORT_DUMMY.typingWritings;
+};
