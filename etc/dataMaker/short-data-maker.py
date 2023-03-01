f1bd6237e7e828ac8d57c483a97b57e5f9abc0ce
jhbaik1501 <jhbaik1501@gmail.com>
2023-03-02 02:44:00 +0900

    fix: 영어 글자 깨짐 문제 해결

--- etc/dataMaker/short-data-maker.py
+++ etc/dataMaker/short-data-maker.py
@@ -10,11 +10,9 @@ while True:
     line = line.strip()
     if not line: break
     
-    data = '''
-    INSERT INTO TYPING (CONTENT, CONTENT_TYPE, LANGUAGE, LENGTH, VIEW_COUNT) 
-    VALUES ('{0}', false, '{1}', {2}, 0);'''.format(line, language, len(line))
+    data = '''INSERT INTO TYPING (CONTENT, CONTENT_TYPE, LANGUAGE, LENGTH, VIEW_COUNT) VALUES ('{0}', false, '{1}', {2}, 0);'''.format(line, language, len(line))
 
-    fw.write(data)
+    fw.write(data.encode('unicode_escape').decode('utf-8') + '\n')
 
 f.close()
 fw.close()
