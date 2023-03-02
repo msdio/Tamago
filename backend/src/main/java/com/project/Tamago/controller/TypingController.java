65db974203946d55ecb4608773f99a6c4610e900
jhbaik1501 <jhbaik1501@gmail.com>
2023-03-03 02:21:26 +0900

    fix: short-typing parameter 추가 로직 변경 및 에러처리 추가

--- backend/src/main/java/com/project/Tamago/controller/TypingController.java
+++ backend/src/main/java/com/project/Tamago/controller/TypingController.java
@@ -1,10 +1,15 @@
 package com.project.Tamago.controller;
 
+import java.util.stream.Stream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Tamago.dto.responseDto.ShortTypingListResDto;
+import com.project.Tamago.exception.CustomException;
+import com.project.Tamago.exception.exceptionHandler.ErrorCode;
 import com.project.Tamago.service.ShortTypingService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,7 +22,12 @@ public class TypingController {
 	private final ShortTypingService shortTypingService;
 
 	@GetMapping("/short-typing")
-	public ShortTypingListResDto findShortTypings() {
-		return shortTypingService.findRandomShortTyping();
+	public ShortTypingListResDto findShortTypings(@RequestParam String language) {
+
+		if (Stream.of("korean", "english", "code")
+			.noneMatch(element -> element.equals(language)))
+			throw new CustomException(ErrorCode.INVALID_PARAMETER);
+
+		return shortTypingService.findRandomShortTyping(language);
 	}
 }
