6c1c8d34ce16652a4d77f970e2a94da91492a4a6
wonseok <tkddls23@konkuk.ac.kr>
2023-03-03 04:11:34 +0900

    feat: 긴글 목록 api 구현

---
+++ backend/src/main/generated/com/project/Tamago/dto/mapper/DataMapperImpl.java
@@ -0,0 +1,37 @@
+package com.project.Tamago.dto.mapper;
+
+import com.project.Tamago.domain.LongTyping;
+import com.project.Tamago.dto.responseDto.LongTypingResDto;
+import com.project.Tamago.dto.responseDto.LongTypingResDto.LongTypingResDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-03T04:07:57+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Eclipse Adoptium)"
+)
+@Component
+public class DataMapperImpl implements DataMapper {
+
+    @Override
+    public LongTypingResDto LongTypingToLongTypingResDto(LongTyping longTyping) {
+        if ( longTyping == null ) {
+            return null;
+        }
+
+        LongTypingResDtoBuilder longTypingResDto = LongTypingResDto.builder();
+
+        longTypingResDto.typingId( longTyping.getId() );
+        longTypingResDto.title( longTyping.getTitle() );
+        longTypingResDto.thumbnail( longTyping.getThumbnail() );
+        if ( longTyping.getTotalPage() != null ) {
+            longTypingResDto.totalPage( String.valueOf( longTyping.getTotalPage() ) );
+        }
+        longTypingResDto.viewCount( longTyping.getViewCount() );
+
+        longTypingResDto.language( longTyping.getLanguage().toString() );
+
+        return longTypingResDto.build();
+    }
+}
--- backend/src/main/java/com/project/Tamago/controller/TypingController.java
+++ backend/src/main/java/com/project/Tamago/controller/TypingController.java
@@ -1,10 +1,15 @@
 package com.project.Tamago.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.Tamago.dto.CustomResponse;
+import com.project.Tamago.dto.responseDto.LongTypingResDto;
 import com.project.Tamago.dto.responseDto.ShortTypingListResDto;
+import com.project.Tamago.service.LongTypingService;
 import com.project.Tamago.service.ShortTypingService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,9 +20,16 @@ import lombok.RequiredArgsConstructor;
 public class TypingController {
 
 	private final ShortTypingService shortTypingService;
+	private final LongTypingService longTypingService;
 
 	@GetMapping("/short")
 	public ShortTypingListResDto findShortTypings() {
 		return shortTypingService.findRandomShortTyping();
 	}
+
+	@GetMapping("/long")
+	public CustomResponse<List<LongTypingResDto>> findLongTypings() {
+		return new CustomResponse<>(longTypingService.findLongTypings());
+	}
+
 }
