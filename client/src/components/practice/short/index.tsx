c176d8e7f83faa473ab53dc25e9a6c9170f5830c
sumi-0011 <selina2000@naver.com>
2023-03-03 15:24:05 +0900

    feat: 경과 시간 측정 start

--- client/src/components/practice/short/index.tsx
+++ client/src/components/practice/short/index.tsx
@@ -5,11 +5,11 @@ import type { ShortTypingResponseType } from '@/apis/typing';
 import CurrentTyping from '@/components/practice/short/CurrentTyping';
 import InfoBar from '@/components/practice/short/InfoBar';
 import PrevTyping from '@/components/practice/short/PrevTyping';
+import useStopwatch from '@/components/practice/short/useStopWatch';
 import DownArrow from '@/icons/DownArrow';
 import TwoRightArrow from '@/icons/TwoRightArrow';
 
 const INIT_INFO = {
-  time: 8,
   wpm: 0,
   accuracy: 0,
   typist: 0,
@@ -21,6 +21,26 @@ type PracticeShortProps = ShortTypingResponseType;
 export default function PracticeShort({ typingWritings }: PracticeShortProps) {
   const [currentIdx, setcurrentIdx] = useState(0);
   const [infos, setInfos] = useState(INIT_INFO);
+
+  const { time, status, timePlay, timePause } = useStopwatch();
+
+  const handleStartTyping = () => {
+    if (status === 'stop') {
+      timePlay();
+    }
+  };
+
+  const handleEndTyping = async () => {
+    timePause();
+  };
+
+  // useEffect(() => {
+  //   timePlay();
+
+  //   return () => {
+  //     timePause();
+  //   };
+  // }, []);
 
   return (
     <Box p='35px 120px' minW='1100px'>
@@ -44,7 +64,7 @@ export default function PracticeShort({ typingWritings }: PracticeShortProps) {
             <DownArrow />
           </Flex>
 
-          <InfoBar {...infos} />
+          <InfoBar {...infos} time={time.second} />
         </Box>
       </Flex>
 
@@ -52,7 +72,7 @@ export default function PracticeShort({ typingWritings }: PracticeShortProps) {
         <Box position='relative' border='0.6px solid #000000' borderRadius='30px 0px 0px 0px' mt='28px'>
           <PrevTyping />
 
-          <CurrentTyping writing={typingWritings[currentIdx]} />
+          <CurrentTyping writing={typingWritings[currentIdx]} onStart={handleStartTyping} onEnd={handleEndTyping} />
 
           <Flex p='24px 40px 32px' alignItems='center' gap='16.18px'>
             <TwoRightArrow />
