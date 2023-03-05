f5388a87a0acf84f69f859102d155c57d817a598
wonseok <tkddls23@konkuk.ac.kr>
2023-03-05 18:02:13 +0900

    refactor: 긴글 샘플 데이터 생성 sql 문 리팩토링

--- backend/src/main/generated/com/project/Tamago/dto/mapper/DataMapperImpl.java
+++ backend/src/main/generated/com/project/Tamago/dto/mapper/DataMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-03T04:07:57+0900",
+    date = "2023-03-03T21:20:26+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Eclipse Adoptium)"
 )
 @Component
--- etc/dataMaker/sample_long_typing._data.sql
+++ etc/dataMaker/sample_long_typing._data.sql
@@ -1,44 +1,48 @@
 -- Long Typing Data
-INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
+INSERT INTO long_typing (title, content, thumbnail, language, length, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('애국가 1절', '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세', SUBSTRING(content, 1, 10),
-        'KOREAN', CHAR_LENGTH(REPLACE(content, '\n', '')), ceiling(length / 150.0), 0, now(), now());
+VALUES (''애국가 1절'', ''동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세'', SUBSTRING(content, 1, 50),
+        ''KOREAN'', CHAR_LENGTH(content), ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0,
+        now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('애국가 2절', '남산위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리기상 일세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세', SUBSTRING(content, 1, 10),
-        'KOREAN', CHAR_LENGTH(REPLACE(content, '\n', '')), ceiling(length / 150.0), 0, now(), now());
+VALUES (''애국가 2절'', ''남산위에 저 소나무 철갑을 두른듯 바람서리 불변함은 우리기상 일세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세'', SUBSTRING(content, 1, 50),
+        ''KOREAN'', CHAR_LENGTH(content), ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0,
+        now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('애국가 3절', '가을하늘 공활한데 높고 구름없이 밝은달은 우리가슴 일편단심일세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세', SUBSTRING(content, 1, 10),
-        'KOREAN', CHAR_LENGTH(REPLACE(content, '\n', '')), ceiling(length / 150.0), 0, now(), now());
+VALUES (''애국가 3절'', ''가을하늘 공활한데 높고 구름없이 밝은달은 우리가슴 일편단심일세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세'', SUBSTRING(content, 1, 50),
+        ''KOREAN'', CHAR_LENGTH(content), ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0,
+        now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('애국가 4절', '이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라사랑하세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세', SUBSTRING(content, 1, 10),
-        'KOREAN', CHAR_LENGTH(REPLACE(content, '\n', '')), ceiling(length / 150.0), 0, now(), now());
+VALUES (''애국가 4절'', ''이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라사랑하세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세'', SUBSTRING(content, 1, 50),
+        ''KOREAN'', CHAR_LENGTH(content), ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0,
+        now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('Twinkle little star', 'Twinkle, twinkle, little star, How I wonder what you are.
+VALUES (''Twinkle little star'', ''Twinkle, twinkle, little star, How I wonder what you are.
 Up above the world so high,
-Like a diamond in the sky.
+           Like a diamond in the sky.
+        Twinkle, twinkle, little star,
+        How I wonder what you are!
 Twinkle, twinkle, little star,
-How I wonder what you are!
-Twinkle, twinkle, little star,
-How I wonder what you are!
+        How I wonder what you are!
 Up above the world so high,
-Like a Diamond in the sky.
+           Like a Diamond in the sky.
+        Twinkle, twinkle, little star,
+        How I wonder what you are!
 Twinkle, twinkle, little star,
-How I wonder what you are!
-Twinkle, twinkle, little star,
-How I wonder what you are!', SUBSTRING(content, 1, 10), 'ENGLISH', CHAR_LENGTH(REPLACE(content, '\n', '')),
-        ceiling(length / 150.0), 0, now(), now());
+        How I wonder what you are!'', SUBSTRING(content, 1, 50), ''ENGLISH'', CHAR_LENGTH(content),
+        ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0, now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('시간이 들겠지', '지나간 여름 밤 시원한 가을바람
+VALUES (''시간이 들겠지'', ''지나간 여름 밤 시원한 가을바람
 난 여전히 잠에 들기가 쉽지않아
 뒤척이고 있어
 내가 계획했던 것
@@ -100,50 +104,54 @@ It takes time
 또 시간이 들겠지
 It takes time
 It takes time
-It takes time', SUBSTRING(content, 1, 10), 'KOREAN', CHAR_LENGTH(REPLACE(content, '\n', '')),
-        ceiling(length / 150.0), 0, now(), now());
+It takes time'', SUBSTRING(content, 1, 50), ''KOREAN'', CHAR_LENGTH(content),
+        ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''\n'', '''')) + 1) / 20.0), 0, now(), now());
 
 INSERT INTO long_typing (title, content, thumbnail, language, LENGTH, total_page, VIEW_COUNT, created_date,
                          updated_date)
-VALUES ('Stack', 'public class Stack {
+VALUES (''Stack'', ''public class Stack {
     private int[] data;
-    private int top;
+private
+int top;
 
-    public Stack(int capacity) {
+public Stack(int capacity) {
         data = new int[capacity];
-        top = -1;
-    }
+        top
+= -1;
+}
 
     public void push(int value) {
         if (top == data.length - 1) {
             throw new RuntimeException("Stack is full");
-        }
+}
         top++;
-        data[top] = value;
-    }
+        data
+[top] = value;
+}
 
     public int pop() {
         if (top == -1) {
             throw new RuntimeException("Stack is empty");
-        }
+}
         int value = data[top];
         top--;
-        return value;
-    }
+return value;
+}
 
     public int peek() {
         if (top == -1) {
             throw new RuntimeException("Stack is empty");
-        }
+}
         return data[top];
-    }
+}
 
     public boolean isEmpty() {
         return top == -1;
-    }
+}
 
     public boolean isFull() {
         return top == data.length - 1;
-    }
-}', SUBSTRING(content, 1, 10),
-        'JAVA', CHAR_LENGTH(REPLACE(content, '\n', '')), ceiling(length / 150.0), 0, now(), now());
+}
+}'', SUBSTRING(content, 1, 50),
+        ''JAVA'',  CHAR_LENGTH(content), ceiling((LENGTH - CHAR_LENGTH(REPLACE(content, ''
+\n'', '''')) + 1)/20.0), 0, now(), now());
