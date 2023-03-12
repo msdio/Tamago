e0c97d56c7a0824cc53c959a3f162ba48a3b0eb2
sumi-0011 <selina2000@naver.com>
2023-03-13 01:24:18 +0900

    fix: padStart 이용하도록 수정

--- client/src/utils/time.ts
+++ client/src/utils/time.ts
@@ -1,9 +1,14 @@
+const toFixedDoubleDigits = (num: number) => {
+  const fixedNumStr = String(parseInt(String(num.toFixed(2))));
+  return fixedNumStr.padStart(2, '0');
+};
+
 export const getSecondToMMSSFormat = (seconds: number) => {
   //3항 연산자를 이용하여 10보다 작을 경우 0을 붙이도록 처리 하였다.
-  const min = parseInt(String((seconds % 3600) / 60));
-  const minFormat = min < 10 ? '0' + min : min;
-  const secFormat = seconds % 60 < 10 ? '0' + (seconds % 60) : seconds % 60;
+  const min = (seconds % 3600) / 60;
+  const sec = seconds % 60;
+  const minFormat = toFixedDoubleDigits(min);
+  const secFormat = toFixedDoubleDigits(sec);
 
-  //연산한 값을 화면에 뿌려주는 코드
   return `${minFormat}:${secFormat}`;
 };
