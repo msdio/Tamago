a148488d799eb4a7bd554788a33b40a7af3a5c5d
Yunu <gyunu47@gmail.com>
2023-03-12 17:50:46 +0900

    [#156] design: 긴 글 출력 높이 고정 해제

--- client/src/components/practice/long/Content/index.tsx
+++ client/src/components/practice/long/Content/index.tsx
@@ -8,14 +8,7 @@ import PracticeLongLayout from '../Layout';
 export default function LongContent({ content, currentPage, language, title, totalPage, typingId }: LongTypingDetail) {
   return (
     <PracticeLongLayout>
-      <Flex
-        direction='column'
-        h='390px'
-        border='0.6px solid #000000'
-        borderRadius='10px'
-        backgroundColor='#fff'
-        p='34px 53px'
-      >
+      <Flex direction='column' border='0.6px solid #000000' borderRadius='10px' backgroundColor='#fff' p='34px 53px'>
         {content.split('\n').map((line, i) => (
           <TypingLine key={i} contentLine={line} states={'c'.repeat(line.length)} />
         ))}
