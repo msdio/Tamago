801f4a340c1e34c4ea98d063f90b93b4b4efb5b4
sumi-0011 <selina2000@naver.com>
2023-03-14 17:49:11 +0900

    fix: fillZeros으로 변경

--- client/src/utils/time.ts
+++ client/src/utils/time.ts
@@ -1,14 +1,13 @@
-const toFixedDoubleDigits = (num: number) => {
-  const fixedNumStr = String(parseInt(String(num.toFixed(2))));
-  return fixedNumStr.padStart(2, '0');
+const fillZeros = ({ num, length }: { num: number; length: number }) => {
+  const fixedNumStr = String(parseInt(String(num.toFixed(length))));
+  return fixedNumStr.padStart(length, '0');
 };
 
 export const getSecondToMMSSFormat = (seconds: number) => {
-  //3항 연산자를 이용하여 10보다 작을 경우 0을 붙이도록 처리 하였다.
   const min = (seconds % 3600) / 60;
   const sec = seconds % 60;
-  const minFormat = toFixedDoubleDigits(min);
-  const secFormat = toFixedDoubleDigits(sec);
+  const minFormat = fillZeros({ num: min, length: 2 });
+  const secFormat = fillZeros({ num: sec, length: 2 });
 
   return `${minFormat}:${secFormat}`;
 };
