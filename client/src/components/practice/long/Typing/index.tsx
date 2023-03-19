c35280e3d5a5297d91e0a860557a72a45df8d4aa
Yunu <gyunu47@gmail.com>
2023-03-19 18:26:21 +0900

    [#176] feat: 긴 글 조회 또는 타이핑 중 나가기 버튼

--- client/src/components/practice/long/Typing/index.tsx
+++ client/src/components/practice/long/Typing/index.tsx
@@ -12,6 +12,7 @@ import { getTypingAccuracy, getTypingSpeed, getTypingWpm, slicedContentAndString
 
 import useStopwatch from '../../short/useStopWatch';
 import PracticeLongLayout from '../Layout';
+import TypingExit from '../TypingExit';
 import TypingLine from '../TypingLine';
 import TypingPagination from '../TypingPagination';
 import InfoBar from './InfoBar';
@@ -201,20 +202,25 @@ export default function PracticeLongTyping({
       <Flex gap='24px' mb='28px'>
         <Box w='118px' bg='#CEF0FF' border=' 0.6px solid #000000' borderRadius={10}></Box>
         <Box flex={1}>
-          <Flex
-            mb='21px'
-            alignItems='center'
-            gap='8.5px'
-            border='0.6px solid #000000'
-            bg='#BCF075'
-            w='fit-content'
-            p='10px 23px'
-            borderRadius={30}
-          >
-            <Text fontSize='18px' fontWeight={500}>
-              긴 글 연습모드
-            </Text>
-            <DownArrow />
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Flex
+              mb='21px'
+              alignItems='center'
+              gap='8.5px'
+              border='0.6px solid #000000'
+              bg='#BCF075'
+              w='fit-content'
+              p='10px 23px'
+              borderRadius={30}
+            >
+              <Text fontSize='18px' fontWeight={500}>
+                긴 글 연습모드
+              </Text>
+              <DownArrow />
+            </Flex>
+            <Box mb='21px'>
+              <TypingExit />
+            </Box>
           </Flex>
           <InfoBar
             accuracy={typingAccuracy.current}
