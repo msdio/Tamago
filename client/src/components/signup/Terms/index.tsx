b8b56ee02aba0b554772035bbfdd539ba5343461
msdio <rlagofka1@naver.com>
2023-03-22 16:37:10 +0900

    [#195] fix: 회원가입 약관 동의 페이지

--- client/src/components/signup/Terms/index.tsx
+++ client/src/components/signup/Terms/index.tsx
@@ -1,15 +1,13 @@
 import { Button, Divider, Flex, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
+import type { ChangeEvent } from 'react';
 import { useState } from 'react';
 
 import { CustomCheckbox } from '@/components/common/Checkbox';
-import IconCheckbox from '@/components/signup/Terms/IconCheckbox';
 import { RightArrow } from '@/icons/RightArrow';
-import { SIGNUP_FORM_PATH } from '@/utils/paths';
+import { LOGIN_PATH, SIGNUP_FORM_PATH } from '@/utils/paths';
 
 export default function SignupTerms() {
-  const router = useRouter();
-
   const [checkedTerms, setCheckedTerms] = useState({
     age: false,
     service: false,
@@ -21,65 +19,70 @@ export default function SignupTerms() {
   const allChecked = Object.values(checkedTerms).every(Boolean);
   const requiredChecked = age && service && privacy;
 
-  // const handleCheckboxChange = () => {
-  //   const { checked, name } = e.target;
-  //   if (name === 'all') {
-  //     setCheckedTerms({ age: checked, service: checked, privacy: checked, marketing: checked });
-  //     return;
-  //   }
-  //   setCheckedTerms({
-  //     ...checkedTerms,
-  //     [name]: e.target.checked,
-  //   });
-  // };
-
-  const handleSubmit = () => {
-    router.push({ pathname: SIGNUP_FORM_PATH, query: { termsMarketing: true } });
-    setCheckedTerms({ age: false, service: false, privacy: false, marketing: false }); //빌드용 임시코드
+  const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { checked, name } = e.target;
+    if (name === 'all') {
+      setCheckedTerms({ age: checked, service: checked, privacy: checked, marketing: checked });
+      return;
+    }
+    setCheckedTerms({
+      ...checkedTerms,
+      [name]: e.target.checked,
+    });
   };
 
   return (
     <Flex direction='column' w='full' gap='16px'>
-      <CustomCheckbox
-        name='all'
-        labelText='모두 동의합니다.'
-        isChecked={allChecked}
-        // onChange={handleCheckboxChange}
-      />
+      <CustomCheckbox name='all' labelText='모두 동의합니다.' isChecked={allChecked} onChange={handleCheckboxChange} />
       <Divider borderColor='gray.main' />
       <CustomCheckbox
         name='age'
         labelText='[필수] 만 14세 이상입니다.'
         isChecked={age}
-        // onChange={handleCheckboxChange}
+        onChange={handleCheckboxChange}
       />
-      <IconCheckbox
-        name='service'
-        labelText='[필수] 서비스 이용약관 동의'
-        isChecked={service}
-        // onChange={handleCheckboxChange}
-        icon={<RightArrow />}
-      />
-      <IconCheckbox
-        name='privacy'
-        labelText='[필수] 개인정보 수집 및 이용 동의'
-        isChecked={privacy}
-        // onChange={handleCheckboxChange}
-        icon={<RightArrow />}
-      />
-      <IconCheckbox
-        name='marketing'
-        labelText='[선택] 마케팅 수신 동의'
-        isChecked={marketing}
-        // onChange={handleCheckboxChange}
-        icon={<RightArrow />}
-      />
-      <Button size='lg' type='submit' mt='32px' isDisabled={!requiredChecked} onClick={handleSubmit}>
-        동의하고 진행하기
-      </Button>
+      <Flex alignItems='center' justify='space-between'>
+        <CustomCheckbox
+          name='service'
+          labelText='[필수] 서비스 이용약관 동의'
+          isChecked={service}
+          onChange={handleCheckboxChange}
+        />
+        <RightArrow />
+      </Flex>
+      <Flex alignItems='center' justify='space-between'>
+        <CustomCheckbox
+          name='privacy'
+          labelText='[필수] 서비스 이용약관 동의'
+          isChecked={privacy}
+          onChange={handleCheckboxChange}
+        />
+        <RightArrow />
+      </Flex>
+      <Flex alignItems='center' justify='space-between'>
+        <CustomCheckbox
+          name='marketing'
+          labelText='[필수] 서비스 이용약관 동의'
+          isChecked={marketing}
+          onChange={handleCheckboxChange}
+        />
+        <RightArrow />
+      </Flex>
+      <Link
+        href={{
+          pathname: SIGNUP_FORM_PATH,
+          query: { termsMarketing: true },
+        }}
+      >
+        <Button size='lg' type='submit' mt='32px' isDisabled={!requiredChecked}>
+          동의하고 진행하기
+        </Button>
+      </Link>
       <Flex gap='14px' justifyContent='center' mt='12px'>
         <Text color='gray.dark'>이미 가입된 계정이 있으신가요?</Text>
-        <Text color='black.dark'>로그인하기</Text>
+        <Link href={LOGIN_PATH}>
+          <Text color='black.dark'>로그인하기</Text>
+        </Link>
       </Flex>
     </Flex>
   );
