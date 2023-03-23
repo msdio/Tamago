aa95e8d1532cde47e497062aef8ade3985719548
jhbaik1501 <jhbaik1501@gmail.com>
2023-03-23 20:36:27 +0900

    fix: user ddl 변경

--- etc/mysql-query/tamago_ddl.sql
+++ etc/mysql-query/tamago_ddl.sql
@@ -33,3 +33,23 @@ create table statistics_all
     wrong_key_info   json        null,
     user_id          int         null
 );
+
+create table user
+(
+    id           int auto_increment
+        primary key,
+    created_date datetime                    null,
+    updated_date datetime                    null,
+    email        varchar(255)                not null,
+    introduce    varchar(20)                 null,
+    nickname     varchar(10)                 not null,
+    password     varchar(255)                not null,
+    profile_img  varchar(255)                null,
+    provider     varchar(255) default 'none' null,
+    provider_id  varchar(255)                null,
+    role         varchar(255)                null,
+    status       bit          default b'1'   null,
+    terms        bit          default b'1'   null,
+    constraint UK_n4swgcf30j6bmtb4l4cjryuym
+        unique (nickname)
+);
