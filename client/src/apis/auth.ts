12ed16d646f4b9b5d11e20b1d271078d59d47040
msdio <rlagofka1@gmail.com>
2023-03-26 22:13:24 +0900

    [#195] fix: 로그인 기능 복원

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -1,5 +1,3 @@
-import { AxiosError } from 'axios';
-
 import { requestWithoutAuth } from '@/apis';
 import type { ApiResponse } from '@/types/apiResponse';
 
@@ -7,24 +5,12 @@ const SIGNUP_PATH = '/auth/join';
 const LOGIN_PATH = '/auth/login';
 const EMAIL_DUPLICATE_PATH = '/auth/email';
 
-export const loginAPI = async (email: string, password: string): Promise<number> => {
-  try {
-    const response = await requestWithoutAuth.post(LOGIN_PATH, { email, password });
+export const loginAPI = async (email: string, password: string): Promise<ApiResponse> => {
+  const { data } = await requestWithoutAuth.post(LOGIN_PATH, { email, password });
 
-    const { data, status } = response;
+  window.localStorage.setItem('accessToken', data.result);
 
-    window.localStorage.setItem('accessToken', data.result);
-
-    return status;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      const { code } = error.response?.data;
-      if (code === 500) {
-        throw new Error('존재하지 않는 회원입니다. '); // TODO: 멘트 수정 필요 (로그인 실패)
-      }
-    }
-    return await Promise.reject(error);
-  }
+  return data;
 };
 
 interface SignupAPIParams {
@@ -34,13 +20,13 @@ interface SignupAPIParams {
 }
 
 export const signupAPI = async ({ email, password, nickname }: SignupAPIParams): Promise<ApiResponse> => {
-  const response = await requestWithoutAuth.post(SIGNUP_PATH, { email, nickname, password });
+  const { data } = await requestWithoutAuth.post(SIGNUP_PATH, { email, nickname, password });
 
-  return response.data;
+  return data;
 };
 
 export const emailDuplicateAPI = async (email: string): Promise<ApiResponse> => {
-  const response = await requestWithoutAuth.get(EMAIL_DUPLICATE_PATH, { params: { email } });
+  const { data } = await requestWithoutAuth.get(EMAIL_DUPLICATE_PATH, { params: { email } });
 
-  return response.data;
+  return data;
 };
