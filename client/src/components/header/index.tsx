cd9429aefdcb52e5a8e8ada4d0765d4db0070914
msdio <rlagofka1@gmail.com>
2023-03-26 20:36:40 +0900

    [#196] fix: header dropdown 나타나는 조건 수정

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-import { LOGIN_PATH, PRACTICE_PATH, PRACTICE_SHORT_PATH, SIGNUP_TERM_PATH } from '@/constants/paths';
+import { LOGIN_PATH, SIGNUP_TERM_PATH } from '@/constants/paths';
 import { TamagoLogo } from '@/icons/TamagoLogo';
 
 import HeaderDropDown from './DropDown';
@@ -39,23 +39,25 @@ export default function Header() {
         zIndex='100'
         background='white.light'
       >
-        <Flex>
+        <Flex h='100%' alignItems='center'>
           <Link href='/'>
             <TamagoLogo />
           </Link>
           <HStack
             spacing='46px'
             w='fit-content'
+            h='100%'
             marginLeft='81px'
             fontSize='17px'
             fontWeight='700'
             onMouseEnter={() => setShowDropDown(true)}
+            onMouseLeave={() => setShowDropDown(false)}
             cursor='pointer'
           >
-            <Text onClick={() => onRouting(PRACTICE_PATH)}>긴글연습</Text>
-            <Text onClick={() => onRouting(PRACTICE_SHORT_PATH)}>짧은글연습</Text>
-            <Text onMouseEnter={() => setShowDropDown(true)}>글등록</Text>
-            <Text onMouseEnter={() => setShowDropDown(true)}>프로필</Text>
+            <Text>긴글연습</Text>
+            <Text>짧은글연습</Text>
+            <Text>글등록</Text>
+            <Text>프로필</Text>
           </HStack>
         </Flex>
         {!isLogin && (
