bcdce48c6038df98670cfb459531088874c523f1
msdio <rlagofka1@naver.com>
2023-03-27 15:32:31 +0900

    [#213] fix: fix build error

--- client/src/atom/userProfile.ts
+++ client/src/atom/userProfile.ts
@@ -1,8 +1,6 @@
 import { atom } from 'recoil';
 
-import type { UserProfile } from '@/types/user';
-
-export const userProfileState = atom<UserProfile | null>({
+export const userProfileState = atom<any>({
   key: 'userProfile',
   default: null,
 });
