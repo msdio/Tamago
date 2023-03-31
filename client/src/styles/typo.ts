b3a14ece74f3e64e7b0ddf04a32ac29892982290
msdio <rlagofka1@gmail.com>
2023-03-31 23:34:49 +0900

    [#241] feat: 디자인 시스템 : typography

---
+++ client/src/styles/typo.ts
@@ -0,0 +1,108 @@
+export const FONT_WEIGHT = {
+  EXTRA_BOLD: 800,
+  BOLD: 'bold',
+  SEMI_BOLD: 600,
+  NORMAL: 'normal',
+  REGULAR: 400,
+};
+
+const textStyles = {
+  'button/medium': {
+    fontSize: '18px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: '100%',
+  },
+  'button/bold': {
+    fontSize: '18px',
+    fontWeight: FONT_WEIGHT.BOLD,
+    lineHeight: '100%',
+  },
+  'button/regular': {
+    fontSize: '14px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: '160%',
+  },
+  'text/hd1x': {
+    fontSize: '30px',
+    fontWeight: FONT_WEIGHT.BOLD,
+    lineHeight: '160%',
+  },
+  'text/hd2': {
+    fontSize: '28px',
+    fontWeight: FONT_WEIGHT.SEMI_BOLD,
+    lineHeight: '160%',
+  },
+  'text/hd3': {
+    fontSize: '25px',
+    fontWeight: FONT_WEIGHT.SEMI_BOLD,
+    lineHeight: '160%',
+  },
+  'text/hd4': {
+    fontSize: '19px',
+    fontWeight: FONT_WEIGHT.SEMI_BOLD,
+    lineHeight: '160%',
+  },
+  'text/Text': {
+    fontSize: '18px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: '160%',
+  },
+  'text/medium': {
+    fontSize: '16px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: '160%',
+  },
+  'text/subtitle': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.SEMI_BOLD,
+    lineHeight: 'auto',
+  },
+  'text/regular': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: '160%',
+  },
+  'text/small': {
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.REGULAR,
+    lineHeight: '100%',
+  },
+  'navigation/medium': {
+    fontSize: '17px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: 'auto',
+  },
+  'text/hover': {
+    fontSize: '17px',
+    fontWeight: FONT_WEIGHT.NORMAL,
+    lineHeight: 'auto',
+  },
+  'point/hd1': {
+    fontFamily: 'GangwonEduPower',
+    fontSize: '94px',
+    fontWeight: FONT_WEIGHT.EXTRA_BOLD,
+    lineHeight: '160%',
+  },
+  'point/hd2': {
+    fontFamily: 'GangwonEduPower',
+    fontSize: '26px',
+    fontWeight: FONT_WEIGHT.EXTRA_BOLD,
+    lineHeight: 'auto',
+  },
+  'point/hd3': {
+    fontFamily: 'GangwonEduPower',
+    fontSize: '20px',
+    fontWeight: FONT_WEIGHT.EXTRA_BOLD,
+    lineHeight: 'auto',
+  },
+  'point/hd4': {
+    fontFamily: 'GangwonEduPower',
+    fontSize: '15px',
+    fontWeight: FONT_WEIGHT.EXTRA_BOLD,
+    lineHeight: 'auto',
+  },
+};
+
+export type TextStyles = typeof textStyles;
+
+export default textStyles;
