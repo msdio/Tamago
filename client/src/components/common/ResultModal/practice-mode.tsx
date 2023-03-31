8e4f1cd8396beb25f3458285788f7038b97eff40
sumi-0011 <selina2000@naver.com>
2023-03-31 15:58:43 +0900

    feat: 준비중인 기능 모달 띄우기

--- client/src/components/common/ResultModal/practice-mode.tsx
+++ client/src/components/common/ResultModal/practice-mode.tsx
@@ -13,11 +13,12 @@ import {
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import Alert from '@/components/common/Alert';
 import IconButton from '@/components/common/IconButton';
 import InfoList from '@/components/common/ResultModal/InfoList';
 import type { TypingResultType } from '@/components/common/ResultModal/types';
+import useToggle from '@/hooks/useToggle';
 import { BookmarkOff } from '@/icons/Heart';
-import Share from '@/icons/Share';
 import { getDateYYYYMMDDHHMMFormat } from '@/utils/time';
 
 interface ResultModalProps {
@@ -31,14 +32,13 @@ interface ResultModalProps {
 
 export default function PracticeResultModal({ onClose, title, isOpen, result, endTime }: ResultModalProps) {
   const router = useRouter();
+
+  const [isPrepareModalOpen, togglePrepareModal] = useToggle();
+
   const isLong = !!title;
 
   const onBookmark = () => {
-    console.log('click Bookmark: ');
-  };
-
-  const onShare = () => {
-    console.log('click Share: ', onShare);
+    togglePrepareModal();
   };
 
   const onReplay = () => {
@@ -47,49 +47,51 @@ export default function PracticeResultModal({ onClose, title, isOpen, result, en
   };
 
   return (
-    // TODO : Modal border radius 수정
-    <Modal isOpen={isOpen} onClose={onReplay} size='md'>
-      <ModalOverlay />
-      <ModalContent m='auto' p='49px 40px 42px'>
-        <ModalHeader p={0}>
-          <Flex justifyContent='space-between'>
-            <Text fontSize='30px' fontWeight={700}>
-              {isLong ? '긴 글' : '짧은 글'} 연습모드 결과
-            </Text>
-            <Flex gap='14px'>
-              <IconButton onAction={onShare} icon={<Share />} />
-              <IconButton onAction={onBookmark} icon={<BookmarkOff />} />
+    <>
+      <Modal isOpen={isOpen} onClose={onReplay} size='md'>
+        <ModalOverlay />
+        <ModalContent m='auto' p='49px 40px 42px'>
+          <ModalHeader p={0}>
+            <Flex justifyContent='space-between'>
+              <Text fontSize='30px' fontWeight={700}>
+                {isLong ? '긴 글' : '짧은 글'} 연습모드 결과
+              </Text>
+              <Flex gap='14px'>
+                {/* <IconButton onAction={onShare} icon={<Share />} /> */}
+                <IconButton onAction={onBookmark} icon={<BookmarkOff />} />
+              </Flex>
             </Flex>
-          </Flex>
-          <Text color='gray.dark' fontSize='14px' fontWeight={500}>
-            {getDateYYYYMMDDHHMMFormat(endTime)}
-          </Text>
-        </ModalHeader>
+            <Text color='gray.dark' fontSize='14px' fontWeight={500}>
+              {getDateYYYYMMDDHHMMFormat(endTime)}
+            </Text>
+          </ModalHeader>
 
-        <ModalBody mt='32px' mb='53px' p={0}>
-          <Flex gap='65px' w='100%'>
-            <Box w='100%'>
-              {title && (
-                <Text fontSize='19px' fontWeight={600} color='black.dark' mb='14px'>
-                  {title}
-                </Text>
-              )}
+          <ModalBody mt='32px' mb='53px' p={0}>
+            <Flex gap='65px' w='100%'>
+              <Box w='100%'>
+                {title && (
+                  <Text fontSize='19px' fontWeight={600} color='black.dark' mb='14px'>
+                    {title}
+                  </Text>
+                )}
 
-              <InfoList {...result} />
-            </Box>
-          </Flex>
-        </ModalBody>
-        <ModalFooter justifyContent='center' p='0' gap='17px'>
-          <Button w='174px' onClick={onReplay}>
-            이어 하기
-          </Button>
-          <Link href='/'>
-            <Button w='174px' variant='outline'>
-              메인화면으로 가기
+                <InfoList {...result} />
+              </Box>
+            </Flex>
+          </ModalBody>
+          <ModalFooter justifyContent='center' p='0' gap='17px'>
+            <Button w='174px' onClick={onReplay}>
+              이어 하기
             </Button>
-          </Link>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+            <Link href='/'>
+              <Button w='174px' variant='outline'>
+                메인화면으로 가기
+              </Button>
+            </Link>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Alert header='준비중인 기능입니다.' isOpen={isPrepareModalOpen} onClose={togglePrepareModal} />
+    </>
   );
 }
