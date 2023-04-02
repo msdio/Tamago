55e83bad7f66271b0578f92abe35ddb94c42e2bd
msdio <rlagofka1@gmail.com>
2023-04-02 10:01:55 +0900

    [#197] feat: 1200px 미만인 경우 서비스 사용 불가능

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import '@/styles/font.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Hide, Show } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
+import Hider from '@/components/common/Hider';
 import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <RecoilRoot>
-        <Component {...pageProps} />
+        <Show breakpoint='(max-width: 1199px)'>
+          <Hider />
+        </Show>
+        <Hide breakpoint='(max-width: 1199px)'>
+          <Component {...pageProps} />
+        </Hide>
       </RecoilRoot>
     </ChakraProvider>
   );
