a5a494146ac5714335318898c2f06246186f0e36
wonseok <tkddls23@konkuk.ac.kr>
2023-04-03 16:23:52 +0900

    feat: 티어 테이블 세팅

---
+++ backend/src/main/java/com/project/Tamago/domain/Tier.java
@@ -0,0 +1,43 @@
+package com.project.Tamago.domain;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.project.Tamago.common.enums.Language;
+import com.project.Tamago.common.enums.Mode;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Tier extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private User user;
+
+	@Enumerated(EnumType.STRING)
+	private Language language;
+
+	private Integer mmr;
+}
