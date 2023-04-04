fd774dd787000ffaf9835e578d2b1a04eefd5a53
Yunu <gyunu47@gmail.com>
2023-04-05 00:21:46 +0900

    feat: 긴 글 제목에 마우스 호버시 썸네일 콘솔 출력

--- client/src/components/typing/long/List/index.tsx
+++ client/src/components/typing/long/List/index.tsx
@@ -77,7 +77,13 @@ export default function PracticeLongList({ currentPage, totalPage, typingList }:
                 <Tr key={typingId}>
                   <Td textAlign='center'>{typingId}</Td>
                   <Td>
-                    <Link href={`${PRACTICE_LONG_PATH_DETAIL}?typingId=${typingId}&pageNum=1&isTyping=true`}>
+                    <Link
+                      href={`${PRACTICE_LONG_PATH_DETAIL}?typingId=${typingId}&pageNum=1&isTyping=true`}
+                      onMouseOver={() => {
+                        // todo: 긴 글 제목에 마우스 호버시 썸네일 표시
+                        console.log(thumbnail);
+                      }}
+                    >
                       {title}
                     </Link>
                   </Td>
