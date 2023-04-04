0c265eb575bb47ac18650210bcb96f92e2e21ab9
sumi-0011 <selina2000@naver.com>
2023-04-04 23:19:24 +0900

    refactor: file naming fix

--- client/src/pages/practice/short/index.tsx
+++ client/src/pages/practice/short/index.tsx
@@ -3,8 +3,8 @@ import { useCallback, useEffect, useState } from 'react';
 
 import type { ShortTypingResultType, TypingLanguageType } from '@/apis/typing';
 import { getShortTypingWritingsAPI } from '@/apis/typing';
-import PracticeShort from '@/components/typing/short';
 import ShortTypingProvider from '@/components/typing/short/_hook/contextShortTyping';
+import PracticeShort from '@/components/typing/short/PracticeShort';
 import useToggle from '@/hooks/useToggle';
 
 export default function PracticeShortPage() {
